a3f6ac18df2f0d4edeb1bfb06f1b4d41a70f1bc1
Jonathan Heinen <heinen@brick-commerce.com>
2015-12-14 10:58:46 +0100

    switch to logback

---
+++ src/main/java/com/brickcommerce/aws/logging/CloudWatchAppender.java
@@ -0,0 +1,143 @@
+package com.brickcommerce.aws.logging;
+
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.logs.model.InputLogEvent;
+import lombok.Setter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CloudWatchAppender extends AppenderBase<ILoggingEvent> {
+
+    // the following four variables reflect the log configuration that
+    // can be set via parameters in the lockback.xml
+    @Setter
+    private int flushPeriod = 10;
+    @Setter
+    private String region;
+    @Setter
+    private String logGroupName = "default";
+    @Setter
+    private String logStreamName;
+    @Setter
+    PatternLayout patternLayout = new PatternLayout();
+
+    private CloudWatchWriter cloudWatchWriter;
+    private ScheduledFuture<?> schedulerFuture;
+
+    /**
+     * Helper method for lazy instantiation of the CloudWatchWriter which is necessary
+     * to prevent the instantiation of loggers during initalisation of log4j
+     *
+     * @return the CloudWatch writer
+     */
+    public CloudWatchWriter getCloudWatchWriter() {
+
+        if (cloudWatchWriter == null) {
+
+            // determine the configuration in the follwing order:
+            // env variables, logback parameters, default values
+
+            String group = System.getProperty("LOG_GROUP_NAME");
+            if (group == null) {
+                group = logGroupName;
+            }
+
+            String stream = System.getProperty("LOG_STREAM_NAME");
+            if (stream == null) {
+                stream = logStreamName;
+            }
+            if (stream == null) {
+                stream = retrieveInstanceId() + "_" + LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+            }
+
+            if (System.getProperty("LOG_REGION") != null) {
+                region = System.getProperty("LOG_GROUP_NAME");
+            }
+            Regions logRegion = null;
+            if (region != null) {
+                logRegion = Regions.fromName(region);
+            }
+
+            final CloudWatchWriter writer = new CloudWatchWriter(group, stream, logRegion);
+            cloudWatchWriter = writer;
+
+            // start scheduler for flushing
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            schedulerFuture = scheduler.scheduleAtFixedRate(writer::flush, flushPeriod, flushPeriod, TimeUnit.SECONDS);
+        }
+        return cloudWatchWriter;
+    }
+
+    /**
+     * Create AWS log event based on the log4j log event and put it to the cloudwatchwriter.
+     */
+    @Override
+    public void append(final ILoggingEvent event) {
+
+        final InputLogEvent awsLogEvent = new InputLogEvent();
+        final long timestamp = event.getTimeStamp();
+
+        awsLogEvent.setTimestamp(timestamp);
+        awsLogEvent.setMessage(patternLayout.doLayout(event));
+
+        getCloudWatchWriter().append(awsLogEvent);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        schedulerFuture.cancel(false);
+        getCloudWatchWriter().flush();
+    }
+
+    /**
+     * Retrieve AWS instance id you are running on.
+     *
+     * @return instanceId the instance id
+     */
+    public static String retrieveInstanceId() {
+
+        final int AWS_CONNECTION_TIMEOUT_MILLIS = 5000;
+        final String AWS_INSTANCE_METADATA_SERVICE_URL = "http://169.254.169.254/latest/meta-data/instance-id";
+
+        String hostId;
+        try {
+            hostId = InetAddress.getLocalHost().getHostName(); // in some unlikely case the AWS metadata service is down
+        } catch (UnknownHostException ux) {
+            hostId = "unknown";
+        }
+        String inputLine;
+        try {
+            final URL EC2MetaData = new URL(AWS_INSTANCE_METADATA_SERVICE_URL);
+            final URLConnection EC2MD = EC2MetaData.openConnection();
+            EC2MD.setConnectTimeout(AWS_CONNECTION_TIMEOUT_MILLIS);
+            final BufferedReader in = new BufferedReader(new InputStreamReader(EC2MD.getInputStream()));
+            while ((inputLine = in.readLine()) != null) {
+                hostId = inputLine;
+            }
+            in.close();
+        } catch (IOException iox) {
+            System.out.println(new SimpleDateFormat("yyyy.MM.dd HH.mm.ss").format(new Date()) +
+                    " CloudWatchAppender: Could not connect to AWS instance metadata service, using " + hostId + " for the hostId.");
+        }
+        return hostId;
+    }
+
+}
---
+++ src/main/java/com/brickcommerce/aws/logging/CloudWatchWriter.java
@@ -0,0 +1,145 @@
+package com.brickcommerce.aws.logging;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.logs.AWSLogsClient;
+import com.amazonaws.services.logs.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by jo on 12.12.15.
+ */
+public class CloudWatchWriter {
+
+    private static final int AWS_DRAIN_LIMIT = 256; // 1MB of 4K messages -- estimate
+    private static final int AWS_LOG_STREAM_MAX_QUEUE_DEPTH = 10000;
+
+    private static Logger logger = LoggerFactory.getLogger(CloudWatchWriter.class);
+
+    private final String logGroupName;
+    private final String logStreamName;
+
+    private AWSLogsClient awsLogsClient;
+
+    private String sequenceTokenCache = null; // aws doc: "Every PutLogEvents request must include the sequenceToken obtained from the response of the previous request.
+    private long lastReportedTimestamp = -1;
+
+    private final BlockingQueue<InputLogEvent> queue = new LinkedBlockingQueue<>(AWS_LOG_STREAM_MAX_QUEUE_DEPTH);
+
+    public CloudWatchWriter(String logGroupName, String logStreamName, Regions region) {
+        this.logGroupName = logGroupName;
+        this.logStreamName = logStreamName;
+
+        logger.debug("Init AWS client");
+        awsLogsClient = new AWSLogsClient(); // this should pull the credentials automatically from the environment
+
+        awsLogsClient.setRegion(Region.getRegion(region));
+
+        createLogGroupAndLogStreamIfNeeded();
+
+    }
+
+    public void append(InputLogEvent awsLogEvent) {
+        queue.add(awsLogEvent);
+    }
+
+    synchronized public void flush() {
+        int drained;
+        final List<InputLogEvent> logEvents = new ArrayList<>(AWS_DRAIN_LIMIT);
+        do {
+            drained = queue.drainTo(logEvents, AWS_DRAIN_LIMIT);
+            if (!logEvents.isEmpty()) {
+
+                fixTimeStamps(logEvents);
+
+                final PutLogEventsRequest putLogEventsRequest = new PutLogEventsRequest(logGroupName, logStreamName, logEvents);
+                putLogEventsRequest.setSequenceToken(sequenceTokenCache);
+                try {
+
+                    final PutLogEventsResult putLogEventsResult = awsLogsClient.putLogEvents(putLogEventsRequest); // 1 MB or 10000 messages AWS cap!
+                    sequenceTokenCache = putLogEventsResult.getNextSequenceToken();
+
+                } catch (final DataAlreadyAcceptedException daae) {
+                    logger.warn("DataAlreadyAcceptedException, will reset the token to the expected one", daae);
+                    sequenceTokenCache = daae.getExpectedSequenceToken();
+                } catch (final InvalidSequenceTokenException iste) {
+                    logger.warn("InvalidSequenceTokenException, will reset the token to the expected one", iste);
+                    sequenceTokenCache = iste.getExpectedSequenceToken();
+                } catch (Exception e) {
+                    logger.error("Error writing logs", e);
+                }
+                logEvents.clear();
+            }
+        } while (drained >= AWS_DRAIN_LIMIT);
+    }
+
+    /**
+     * Ensures that the given List of log events fulfills the needs as purposed by aws cloudwatch logs.
+     * Sorts the events by their timestamp and ensures that all timestamps are newer then the ones already sent
+     * @param logEvents a list of log events
+     */
+    private void fixTimeStamps(List<InputLogEvent> logEvents) {
+
+        Collections.sort(logEvents, (event1, event2) -> event1.getTimestamp().compareTo(event2.getTimestamp()));
+
+        if (lastReportedTimestamp > 0) {
+            for (InputLogEvent event : logEvents) {
+                if (event.getTimestamp() < lastReportedTimestamp)
+                    event.setTimestamp(lastReportedTimestamp);
+                else
+                    break;
+            }
+        }
+
+        lastReportedTimestamp = logEvents.get(logEvents.size() - 1).getTimestamp();
+    }
+
+    /**
+     * Create log group ans log stream if needed.
+     *
+     * @return sequence token for the created stream
+     */
+    private String createLogGroupAndLogStreamIfNeeded() {
+        final DescribeLogGroupsResult describeLogGroupsResult = awsLogsClient.describeLogGroups(new DescribeLogGroupsRequest().withLogGroupNamePrefix(logGroupName));
+        boolean createLogGroup = true;
+        if (describeLogGroupsResult != null && describeLogGroupsResult.getLogGroups() != null && !describeLogGroupsResult.getLogGroups().isEmpty()) {
+            for (final LogGroup lg : describeLogGroupsResult.getLogGroups()) {
+                if (logGroupName.equals(lg.getLogGroupName())) {
+                    createLogGroup = false;
+                    break;
+                }
+            }
+        }
+        if (createLogGroup) {
+            logger.debug("Creating logGroup: " + logGroupName);
+            final CreateLogGroupRequest createLogGroupRequest = new CreateLogGroupRequest(logGroupName);
+            awsLogsClient.createLogGroup(createLogGroupRequest);
+        }
+        String logSequenceToken = null;
+        boolean createLogStream = true;
+        final DescribeLogStreamsRequest describeLogStreamsRequest = new DescribeLogStreamsRequest(logGroupName).withLogStreamNamePrefix(logStreamName);
+        final DescribeLogStreamsResult describeLogStreamsResult = awsLogsClient.describeLogStreams(describeLogStreamsRequest);
+        if (describeLogStreamsResult != null && describeLogStreamsResult.getLogStreams() != null && !describeLogStreamsResult.getLogStreams().isEmpty()) {
+            for (final LogStream ls : describeLogStreamsResult.getLogStreams()) {
+                if (logStreamName.equals(ls.getLogStreamName())) {
+                    createLogStream = false;
+                    logSequenceToken = ls.getUploadSequenceToken();
+                }
+            }
+        }
+
+        if (createLogStream) {
+            logger.debug("Creating logStream: " + logStreamName);
+            final CreateLogStreamRequest createLogStreamRequest = new CreateLogStreamRequest(logGroupName, logStreamName);
+            awsLogsClient.createLogStream(createLogStreamRequest);
+        }
+        return logSequenceToken;
+    }
+}
